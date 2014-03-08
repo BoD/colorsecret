c6e3a9d6e3b8720ff8701e65c8fa08117b453de5
Benoit Lubek <BoD@JRAF.org>
2014-03-08 18:04:54 +0100

    Updated copyright header.

--- src/org/jraf/android/colorsecret/model/Game.java
+++ src/org/jraf/android/colorsecret/model/Game.java
@@ -7,7 +7,7 @@
  *                              /___/
  * repository.
  *
- * Copyright 2011 Benoit 'BoD' Lubek (BoD@JRAF.org).  All Rights Reserved.
+ * Copyright (C) 2011 Benoit 'BoD' Lubek (BoD@JRAF.org)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
